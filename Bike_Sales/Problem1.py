e2a75f1c7080739ff143995957dc15844c5e4b1a
Kruoshin <144993272+Kuroishin-beep@users.noreply.github.com>
2025-02-06 10:48:19 +0800

    added a new Analysis

---
+++ Bike_Sales/Problem1.py
@@ -0,0 +1,10 @@
+# What is the total revenue generated in 2023, and how does it vary across different months?
+
+import pandas as pd
+
+#Load the dataset
+data = pd.read_csv('D:\Github\Project\DataLab\Data_Sets\cleaned_bike_sales_data.csv')
+
+#print the data
+
+print(data.head)
